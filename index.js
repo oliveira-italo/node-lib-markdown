ddcd3241c3864be5bb339001a7cf24c32e6c2413
oliveira-italo <arievilo.olati@gmail.com>
2023-05-29 21:54:17 -0300

    Aula 02
    Importação de libs
    node init
    type module

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('olá mundo');
+import chalk from "chalk";
+
+console.log(chalk.blue.bgWhite('olá mundo'));
+console.log('A interface File provê informações sobre arquivos e permite ao JavaScript  a acessar seu conteúdo.');
+console.log('The book is on the table');
